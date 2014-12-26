a4a74c3b16a87aafe0d5bb215d99a080dace8cd0
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-26 12:58:34 +0000

    - basic structure and necessary resources for Assignment 2

---
+++ trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLP_Assignment2
+{
+	class ParsingModelExtractor
+	{
+		internal void Run()
+		{
+			// TODO: read in dep-cfg & pos-cfg
+			//		 from these files extract a grammar file & lexicon file
+		}
+	}
+}
---
+++ trunk/NLP_Assignment2/NLP_Assignment2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLP_Assignment2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ParsingModelExtractor pme = new ParsingModelExtractor();
+			pme.Run();
+		}
+	}
+}

4f3e89162f76e374e8ae74a2988f44fc934f4bbb
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-26 01:12:51 +0000

    - changes from NLP_Assignment1_20141211_1735

--- trunk/NLP_Assignment1/NLP_Assignment1/LanguageModel.cs
+++ trunk/NLP_Assignment1/NLP_Assignment1/LanguageModel.cs
@@ -25,20 +25,22 @@ namespace NLP_Assignment1
 
 			// extracting a List of words from the training set
             // TODO_HIGH: add an UnknownManager (from Enum) which specifies a rule with which we generate a subset of unk# markers/words when processing the wordList
-			dm.wordList = proc.ExtractWords(dm.LoadFile("../../res/talbanken-dep-train.conll"));
+			dm.wordList = proc.ExtractWords(dm.LoadFile("../../res/talbanken-dep-train.conll"), UnknownHandler.EVERY_NTH_ROW, 10);
+            //dm.wordList = proc.ExtractWords(dm.LoadFile("../../res/talbanken-dep-test.conll"), UnknownHandler.NONE, 10);
 
 			// counting the number of unigrams, bigrams and calculating the probability of bigrams
 			dm.countUnigrams = proc.CountNGrams(dm.wordList, NGram.UNIGRAM);
 			dm.countBigrams = proc.CountNGrams(dm.wordList, NGram.BIGRAM);
+
 			//dm.probListBigrams = proc.CalcProb(dm.countBigrams, dm.countUnigrams, Storage.FLOAT);
 			//dm.probListBigrams_bigrat = proc.CalcProb(dm.countBigrams, dm.countUnigrams, Storage.BIGRAT, Smoothing.NONE);
-            // TODO: check if the sum in logspace is correct, went from -175k without smoothing to -440k when smoothing
-            dm.probListBigrams_bigrat = proc.CalcProb(dm.countBigrams, dm.countUnigrams, Storage.BIGRAT, Smoothing.ADDONE);
+            
+            // NOTE: run CalcProb (...) without smoothing (we want to perform smoothing on the fly)
+            dm.probListBigrams_bigrat = proc.CalcProb(dm.countBigrams, dm.countUnigrams, Storage.BIGRAT, Smoothing.NONE);
 			
 			// calculating perplexity of the training set
 			// TODO: store perplex (in DataManager), use an appropriate datatype
-			dm.perplex = proc.CalcPerplex(dm.countUnigrams, dm.probListBigrams_bigrat, Storage.BIGRAT);
-            Console.WriteLine("perplexity: "+dm.perplex);
+            dm.perplex = proc.CalcPerplex(dm.countUnigrams, dm.probListBigrams_bigrat, Storage.BIGRAT, Smoothing.NONE);
 
 			Console.Read();
 		}

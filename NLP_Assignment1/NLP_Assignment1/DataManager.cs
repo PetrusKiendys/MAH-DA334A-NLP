03ca0f37b4f51e61d472ae92b8c6207f88be8e20
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-26 01:15:03 +0000

    - changes from NLP_Assignment1_20141212_1710 -- added notes for the written explaination of some parts of this assignment -- added an output folder (which holds the text files that is written by the application)

--- NLP_Assignment1/NLP_Assignment1/DataManager.cs
+++ NLP_Assignment1/NLP_Assignment1/DataManager.cs
@@ -19,15 +19,37 @@ namespace NLP_Assignment1
 		internal List<string> wordList;
         internal decimal perplex;
 
+        // TODO_LOW: rename these "test set variables" to something more generic
+        internal List<string> wordListTestSet;
+        internal Dictionary<string, int> countUnigramsTestSet;
+
+        internal List<string> wordDiffList;
+
 		internal string LoadFile(string path)
 		{
 			FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read);
 			StreamReader reader = new StreamReader(file);
 			string res = reader.ReadToEnd().ToLower();  // Word and bigram counts are not case sensitive
-			file.Close();
+            reader.Close();
+            file.Close();
 
 			return res;
 		}
+
+        internal void SaveFile(string path, List<string> content)
+        {
+            FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+
+            // TODO: write content to file via: writer.WriteLine
+            foreach (string entry in content)
+            {
+                writer.WriteLine(entry);
+            }
+
+            writer.Close();
+            file.Close();
+        }
 	
 	}
 }

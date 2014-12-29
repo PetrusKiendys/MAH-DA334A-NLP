2c67be0c878c33fbf57b280074141d8f34655e03
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-29 17:42:38 +0000

    - removed Node.cs - used Azddins code located in ParsingModelExtractor.cs as a basis/utility for implementing own TreeNode parser located in ParsingModelExtractor2.cs method ExtractNode - added formatted cfg-pos sentences for reference in /res/txt - modified and added functionality to TreeNode.cs

--- trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
+++ trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
@@ -11,11 +11,11 @@ namespace NLP_Assignment2
 	{
         public void Run()
         {
-            string filepath = "../../res/talbanken-cfg_pos-train.cfg";
-
+            string filepath = "../../res/data/talbanken-cfg_pos-train.cfg";
             FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(file);
             string output = reader.ReadToEnd();
+            reader.Close();
             file.Close();
             
             string[] rows = output.Split('\n');
@@ -24,27 +24,37 @@ namespace NLP_Assignment2
             {
                 string row = rows[i];
                 Console.WriteLine(row);
-                this.Start(row);
+                Start(row);
+
+                break;
             }
 
             Console.ReadLine();
         }
+
+
         public void Start(string input, string start=null)
         {
-            if (input == null)
-                return;
+            // invoked when input is not surrounded by parenthesis
             if (input[0] != '(' || input[input.Length - 1] != ')')
             {
                 Console.WriteLine(start);
                 return;
             }
+
+            // count is used to determine the extent of a "node"
             int count = 0;
+            // list that contains whole POS tags
             List<string> subStrs = new List<string>();
 
+            // removes the parenthesis
             input = input.Remove(0, 1);
             input = input.Remove(input.Length - 1, 1);
+            
+
             int i = input.IndexOf(' ');
 
+            // removes the parsing of word from reading in words and POS tags
             string nextInput = i > 0 ? input.Substring(0, i) : input;
 
             if (start != null)
@@ -54,6 +64,7 @@ namespace NLP_Assignment2
 
             input = input.Remove(0, i + 1);
 
+            // parsing of a whole POS tag
             string tempStr = "";
             for (int j = 0; j < input.Length; j++)
             {
@@ -70,6 +81,8 @@ namespace NLP_Assignment2
                     }
                 }
             }
+
+
             if (subStrs.Count == 0)
                 subStrs.Add(tempStr);
 

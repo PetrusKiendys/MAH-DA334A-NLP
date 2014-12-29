c271de38e200e2eeb29ba340970595a367bf670f
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-29 10:59:23 +0000

    Azddin's progress with the tree parser (worked on from 2014-12-26 to 2014-12-29)

--- trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
+++ trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
@@ -9,45 +9,75 @@ namespace NLP_Assignment2
 {
 	class ParsingModelExtractor
 	{
-        /*
-        static Array parenthesisFinder(string row)
+        public void Run()
         {
-
-            string pattern = @"\(\d+\)";
-            string[] lines = Regex.Split(row, pattern);
-
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string fline = lines[i];
-
-                if (fline.Contains('('))
-                {
-                    string[] fln = Regex.Split(row, pattern);
-
-                    return parenthesisFinder(fln);
-                }
-
-            }
-
-            return output;
-        }
-        */
-		internal void Run()
-		{
-			// TODO: read in dep-cfg & pos-cfg
-			//		 from these files extract a grammar file & lexicon file
-
             string filepath = "../../res/talbanken-cfg_pos-train.cfg";
 
             FileStream file = new FileStream(filepath, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(file);
             string output = reader.ReadToEnd();
             file.Close();
+            
+            string[] rows = output.Split('\n');
+            
+            for (int i = 0; i < rows.Length; i++)
+            {
+                string row = rows[i];
+                Console.WriteLine(row);
+                this.Start(row);
+            }
 
-            // VBP HPP VB VBP PMP MADP
-            var row = "(SENT (VBP (HPP (HP Vem))(VB kan)(VBP (VB få)(PMP (PM ATP)))(MADP (MAD ?))))";
-            row = row.Substring(6);
+            Console.ReadLine();
+        }
+        public void Start(string input, string start=null)
+        {
+            if (input == null)
+                return;
+            if (input[0] != '(' || input[input.Length - 1] != ')')
+            {
+                Console.WriteLine(start);
+                return;
+            }
+            int count = 0;
+            List<string> subStrs = new List<string>();
 
-		}
-	}
+            input = input.Remove(0, 1);
+            input = input.Remove(input.Length - 1, 1);
+            int i = input.IndexOf(' ');
+
+            string nextInput = i > 0 ? input.Substring(0, i) : input;
+
+            if (start != null)
+                start = start + " -> " + nextInput;
+            else
+                start = nextInput;
+
+            input = input.Remove(0, i + 1);
+
+            string tempStr = "";
+            for (int j = 0; j < input.Length; j++)
+            {
+                tempStr += input[j];
+                if (input[j] == '(')
+                    count++;
+                else if (input[j] == ')')
+                {
+                    count--;
+                    if (count == 0)
+                    {
+                        subStrs.Add(tempStr);
+                        tempStr = "";
+                    }
+                }
+            }
+            if (subStrs.Count == 0)
+                subStrs.Add(tempStr);
+
+            subStrs.ForEach(delegate(string it)
+            {
+                this.Start(it, start);
+            });
+        }
+
+    }
 }

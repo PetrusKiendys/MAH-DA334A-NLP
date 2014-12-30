c7f859e6af1a0c611d91392b81d1ea233a97ccee
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2014-12-30 18:30:55 +0000

    - renamed Azddin's utility code to TreePathingUtility.cs - renamed ParsingModelExtractor2.cs to ParsingModelExtractor.cs - moved camera images that describe erroneous parse trees to /res/img/archive and replaced with new images (some images in /res/img are probably describing erroneous trees and need to be validated or remade) - added the report document NLP Rapport Assignment2.docx into /res/report (will later be replaced by an .rtf document)

--- NLP_Assignment2/NLP_Assignment2/TreeNode.cs
+++ NLP_Assignment2/NLP_Assignment2/TreeNode.cs
@@ -5,39 +5,40 @@ using System.Text;
 
 namespace NLP_Assignment2
 {
-    class TreeNode
-    {
-        internal string Value {get; set;}
-        List<TreeNode> childNodes = new List<TreeNode>();
+	class TreeNode
+	{
+		internal string Value {get; set;}
+		List<TreeNode> childNodes = new List<TreeNode>();
 
-        internal void addChild (TreeNode child)
-        {
-            childNodes.Add(child);
-        }
+		internal void addChild (TreeNode child)
+		{
+			childNodes.Add(child);
+		}
 
-        internal TreeNode getChild (int index)
-        {
-            return childNodes[index];
-        }
+		internal TreeNode getChild (int index)
+		{
+			return childNodes[index];
+		}
 
-        internal bool hasChildren()
-        {
-            if (countChildren() > 0)
-                return true;
-            else
-                return false;
-        }
+		internal bool hasChildren()
+		{
+			if (countChildren() > 0)
+				return true;
+			else
+				return false;
+		}
 
-        internal int countChildren()
-        {
-            return childNodes.Count;
-        }
+		internal int countChildren()
+		{
+			return childNodes.Count;
+		}
 
-        // TODO: this function may need to be altered so that it returns a List that has no references to
-        //       the original List of node children
-        internal List<TreeNode> getChildren()
-        {
-            return new List<TreeNode>(childNodes);
-        }
-    }
+		// FIX: this method needs to be altered so that it returns a new List that has no references to
+		//       the original List of node children as manipulation of the return of this method also changes the properties
+		//		 of the original nodes (the original nodes and their properties are accessible, this should not be the case!)
+		internal List<TreeNode> getChildren()
+		{
+			return new List<TreeNode>(childNodes);
+		}
+	}
 }

cfd8bf9c5af2284f5b9eace638e63562bafd0f45
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2015-01-01 01:17:13 +0000

    - Assignment2 -- broke up ParsingModelExtractor into a three-part architecture (similar to Assignment1) consisting of ParsingModelExtractor.cs which handles the main control flow, DataManager.cs which handles data and Processor.cs which handles the logic of the application -- cleaned up Program.cs -- cleaned up all classes from obsolete using directives -- added a comment for TreePathingUtility.cs, preparing it to be stripped down later on from Run() methods to preserve it for reference purposes only -- added support for enumerators through Enums.cs -- added a backlog (a to-do list for this assignment) in /res/notes

--- trunk/NLP_Assignment2/NLP_Assignment2/Program.cs
+++ trunk/NLP_Assignment2/NLP_Assignment2/Program.cs
@@ -14,15 +14,6 @@ namespace NLP_Assignment2
 
 			// keep the console window open/active at the end of runtime
 			Console.Read();
-
-
-			// Petrus TreeNode testkod
-//			TreeNode rootNode = new TreeNode();
-			
-//            			string example_sentence = "(SENT (RTP (ATP (AT Individuell))(RT beskattning)(ETP (ET av)(PAP (PA arbetsinkomster)))))";
-//			string[] nodes_string = { "SENT", "RTP", "ATP", "AT", "individuell", "RT", "beskattning", "ETP", "ET", "av", "PAP", "PA", "arbetsinkomster" };
-
-//			rootNode.Value = nodes_string[0];
 		}
 	}
 }

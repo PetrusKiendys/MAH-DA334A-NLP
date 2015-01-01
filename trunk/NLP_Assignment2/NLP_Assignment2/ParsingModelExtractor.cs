8b4e62ff1297480f2695d0fa9d89dc7bdda32ba7
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2015-01-01 19:38:39 +0000

    - Assignment2 -- DataManager.cs: added the SaveFile(...) method from Assignment1 -- ParsingModelExtractor.cs: PME is now fully implemented, also added Console output that specifies the different stages of runtime for the user and generalized the Run() method into Run(string file) for running PME with different input files -- Processor.cs: added support for formatting the lexicon ruleset -- Program.cs: instantiating 2 PME objects from Program and calling Run with 2 different arguments which specify the input file to be passed (just to be sure that no overlap or unpurged values remain from the first run), leaving reference test code in, will be removed in the next commit -- adding /out and the grammar and lexicon files that were extracted by running the ParsingModelExtractor application

--- trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
+++ trunk/NLP_Assignment2/NLP_Assignment2/ParsingModelExtractor.cs
@@ -7,23 +7,43 @@ namespace NLP_Assignment2
 {
 	class ParsingModelExtractor
 	{
-		internal void Run()
+		internal void Run(string file)
 		{
 			DataManager dm = new DataManager();
 			Processor proc = new Processor();
+			
+			string[] filename_array = file.Split('/');
+			string filename = filename_array[filename_array.Length-1];
 
-			string data = dm.LoadFile("../../res/data/talbanken-cfg_pos-train.cfg");
+			// load the phrase structure trees
+			Console.WriteLine("Loading the phrase structure tree from "+filename);
+			string data = dm.LoadFile(file);
 			string[] sentences = data.Split('\n');
 			sentences = sentences.Take(sentences.Count()-1).ToArray();	// getting rid of the last empty row in the data file
 
+			// extract rules from the structure trees
+			Console.WriteLine("Extracting rules from phrase structure trees...");
 			proc.ExtractRules(sentences, dm.grammarRules, ExtractMode.GRAMMAR);
-			//string[] tempSentences = {"(SENT (NNP (JJP (JJ Individuell))(NN beskattning)(PPP (PP av)(NNP (NN arbetsinkomster)))))"};
-			//proc.ExtractRules(tempSentences, ExtractMode.LEXICON, dm.lexiconRules);
 			proc.ExtractRules(sentences, dm.lexiconRules, ExtractMode.LEXICON);
 
-			// format grammar & lexicon
+			// format grammar and lexicon
+			Console.WriteLine("Formatting grammar and lexicon lists...");
 			dm.formattedGrammarRules = proc.FormatRules(dm.grammarRules, ExtractMode.GRAMMAR);
 			dm.formattedLexiconRules = proc.FormatRules(dm.lexiconRules, ExtractMode.LEXICON);
+
+			// save the grammar and lexicon rules to text files
+			if (file.Contains("pos"))
+			{
+				dm.SaveFile("../../out/grammar-pos.txt", dm.formattedGrammarRules);
+				dm.SaveFile("../../out/lexicon-pos.txt", dm.formattedLexiconRules);
+			}
+			else if (file.Contains("dep"))
+			{
+				dm.SaveFile("../../out/grammar-dep.txt", dm.formattedGrammarRules);
+				dm.SaveFile("../../out/lexicon-dep.txt", dm.formattedLexiconRules);
+			}
+			Console.WriteLine("The grammar and lexicon files have been successfully created for "+filename+"\n");
+			
 		}
 	}
 }
--- trunk/NLP_Assignment2/NLP_Assignment2/Program.cs
+++ trunk/NLP_Assignment2/NLP_Assignment2/Program.cs
@@ -9,11 +9,30 @@ namespace NLP_Assignment2
 	{
 		static void Main(string[] args)
 		{
-			ParsingModelExtractor pme = new ParsingModelExtractor();
-			pme.Run();
+			ParsingModelExtractor pme1 = new ParsingModelExtractor();
+			pme1.Run("../../res/data/talbanken-cfg_pos-train.cfg");
+			ParsingModelExtractor pme2 = new ParsingModelExtractor();	// NOTE_LOW: not sure if another instance of PME needs to be created but it is done just in case, fix this if needed
+			pme2.Run("../../res/data/talbanken-cfg_dep-train.cfg");
 
-			// keep the console window open/active at the end of runtime
-			Console.Read();
+			//		NOTE: test code for FormatRules with ExtractMode.LEXICON
+			//		TODO: commit this for reference purposes and later remove
+			//List<string> list = new List<string>();
+			//list.Add("XXX YYY ZZZ");
+			//list.Add("AAA BBB CCC");
+			//list.Add("AAA XXX CCC");
+			//Console.WriteLine(list.Foo("XXX", ' '));	// should return 0 (index of element) or element of list
+			//Console.WriteLine(list.Foo("YYY", ' '));	// should return -1 or null
+			//Console.WriteLine(list.Foo("AAA", ' '));	// should return 1 (index of element) or element of list
+			//Console.WriteLine(list.Foo("DDD", ' '));	// should return -1 or null
+
+			//string searchstr = "AAA ";
+			//string str = list.FirstOrDefault(x => x.StartsWith(searchstr));
+			//int i = list.IndexOf(str);
+
+			//Console.WriteLine("index of "+"\""+searchstr+"\": "+i);
+
+			Console.WriteLine("Press Enter to close this window...");
+			Console.Read();		// keep the console window open/active at the end of runtime
 		}
 	}
 }

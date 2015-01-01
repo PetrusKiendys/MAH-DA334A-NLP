ecfb2f6875bccbba6e1a7aacec8b6ebb9fa2e23d
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2015-01-01 21:20:21 +0000

    - Assignment2 -- moved external software into their own folders -- Program.cs: removed test code -- /external_software/BitPar: structured input files, output files and created batch files for running BitPar -- res/notes: added links & notes.txt which contains useful links for completing this assignment

---
+++ trunk/NLP_Assignment2/external_software/BitPar/parse-dep.bat
@@ -0,0 +1,2 @@
+bitpar -v in/grammar-dep.txt in/lexicon-dep.txt in/test_input.txt out/output-dep-bitpar.txt
+start out
---
+++ trunk/NLP_Assignment2/external_software/BitPar/parse-pos.bat
@@ -0,0 +1,2 @@
+bitpar -v in/grammar-pos.txt in/lexicon-pos.txt in/test_input.txt out/output-pos-bitpar.txt
+start out

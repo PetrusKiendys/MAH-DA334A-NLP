3976b50ff819ea94d87a610146b20feb2f436626
petruskiendys@gmail.com <petruskiendys@gmail.com@5c61e97d-1391-d8ea-f88c-e61f5c4e52f8>
2015-01-06 12:40:39 +0000

    - Assignment2 -- fixed batch files for usage with all "external software" -- also fixed the naming of the I/O files used by "external software" -- added a suggestion of renaming ParsingModelExtractor.cs to something else (to provide more clarity as to how ParsingModelExtractor is used and what it does and produces) -- Processor.cs: renamed the placeholder variable "x" to "elem" -- updated backlog.txt, added an extra task -- added runall.bat, may fully implement it if there's time -- wrote the remaining formatted phrase structure trees, located in /res/txt

--- trunk/NLP_Assignment2/external_software/Evalb/evalb.bat
+++ trunk/NLP_Assignment2/external_software/Evalb/evalb.bat
@@ -1,1 +1,2 @@
-evalb talbanken-cfg_dep-test.cfg outdep.txt > evalb-dep.txt
+evalb in/talbanken-cfg_pos-test.cfg in/postprocess-out-pos.txt > out/evalb-out-pos.txt
+evalb in/talbanken-cfg_dep-test.cfg in/postprocess-out-dep.txt > out/evalb-out-dep.txt
---
+++ trunk/NLP_Assignment2/external_software/post-process/post-process.bat
@@ -0,0 +1,2 @@
+perl post-process.pl in/bitpar-out-pos.txt out/postprocess-out-pos.txt
+perl post-process.pl in/bitpar-out-dep.txt out/postprocess-out-dep.txt
